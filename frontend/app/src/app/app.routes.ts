197d75d6303f9bc257d3f2a39c6bafa6aa44ec04
PierreLaGuerre <pierangelo.guerrero@gmail.com>
2026-03-27 09:04:09 +0100

    created frontend app  routes

--- frontend/app/src/app/app.routes.ts
+++ frontend/app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './features/login/login.component';
+import { PlansComponent } from './features/plans/plans.component';
+import { UserPageComponent } from './features/user-page/user-page.component';
+import { CourseDetailComponent } from './features/course-detail/course-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'plans', component: PlansComponent },
+  { path: 'user', component: UserPageComponent },
+  { path: 'courses/:id', component: CourseDetailComponent },
+  { path: '**', redirectTo: '' }
+];
