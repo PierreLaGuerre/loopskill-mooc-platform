372f151da7a9d3c88f277cb225cf6ac411a3fde3
PierreLaGuerre <pierangelo.guerrero@gmail.com>
2026-03-27 10:32:28 +0100

    created explore component and add the route

--- frontend/app/src/app/app.routes.ts
+++ frontend/app/src/app/app.routes.ts
@@ -1,17 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
-import { PlansComponent } from './features/plans/plans.component';
-import { CourseDetailComponent } from './features/course-detail/course-detail.component';
 import { LoginComponent } from './features/login/login.component';
+import { PlansComponent } from './features/plans/plans.component';
 import { UserPageComponent } from './features/user-page/user-page.component';
+import { CourseDetailComponent } from './features/course-detail/course-detail.component';
+import { ExploreComponent } from './features/explore/explore.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'explore', component: HomeComponent },
-  { path: 'my-learnings', component: UserPageComponent },
-  { path: 'plans', component: PlansComponent },
-  { path: 'profile', component: UserPageComponent },
+  { path: 'explore', component: ExploreComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'plans', component: PlansComponent },
+  { path: 'user', component: UserPageComponent },
   { path: 'courses/:id', component: CourseDetailComponent },
   { path: '**', redirectTo: '' }
 ];
