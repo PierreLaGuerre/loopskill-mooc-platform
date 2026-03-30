4daf623ddb3c18741d5586019b666c1613314d3c
PierreLaGuerre <pierangelo.guerrero@gmail.com>
2026-03-30 14:27:18 +0200

    fix: navbar and footer realigned

--- frontend/app/src/app/core/layout/navbar/navbar.component.ts
+++ frontend/app/src/app/core/layout/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
+import { MOCK_USER } from '../../mocks/mock-user';
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -9,5 +10,5 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  userInitial: string = MOCK_USER.name.charAt(0).toUpperCase();
 }
