0b7d3448068d2485118dd7b689d72216c3bedd44
PierreLaGuerre <pierangelo.guerrero@gmail.com>
2026-04-08 09:12:22 +0200

    fix: now category card is a link that open explore working as a filter

--- frontend/app/src/app/shared/components/category-card/category-card.component.ts
+++ frontend/app/src/app/shared/components/category-card/category-card.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 import { Category } from '../../../core/models/category.model';
 
 @Component({
   selector: 'app-category-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './category-card.component.html',
   styleUrl: './category-card.component.scss'
 })
